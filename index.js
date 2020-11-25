c5de884911ea8790471440935d8312b4ea9c3d0b
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-11-25 14:46:49 +0200

    Remove unnecessary code.

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const api = require('./routes/api.js')
 
 app.use('/api', api)
 
-const db = [{ name: 'tiina' }, { name: 'jack' }]
-
-app.get('/names', (req, res) => {
-  res.send(db)
-})
-
 const port = process.env.PORT || 8080
 const server = app.listen(port, () => {
   console.log(`Listening on port ${server.address().port}`)
