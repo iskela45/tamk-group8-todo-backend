508eaf707b6c178a6a56c07aaeb8aa8e42ba0cfe
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-11-25 13:15:56 +0200

    Remove unnecessary coordination validation.

--- validator.js
+++ validator.js
@@ -3,46 +3,18 @@ const validator = new Validator()
 
 const idSchema = {
   type: 'number',
-  minimum: 1
-}
-
-// Check that the given variable is an object
-// that holds keys latitude and longitude
-// with numbers between given ranges.
-const cordSchema = {
-  required: ['latitude', 'longitude'],
-  type: 'object',
-  properties: {
-    latitude: {
-      type: 'number',
-      minimum: -90,
-      maximum: 90
-    },
-    longitude: {
-      type: 'number',
-      minimum: -180,
-      maximum: 180
-    }
-  }
+  minimum: 1,
 }
 
 const olio = {
-
-  idValidator (id) {
-    function asyncOp (resolve, reject) {
+  // Validate id through JSON schema that id is a number and over 1
+  idValidator(id) {
+    function asyncOp(resolve, reject) {
       const validation = validator.validate(id, idSchema)
       resolve(validation)
     }
-    return new Promise(asyncOp).catch(err => console.log(err))
+    return new Promise(asyncOp).catch((err) => console.log(err))
   },
-
-  cordValidator (cords) {
-    function asyncOp (resolve, reject) {
-      const validation = validator.validate(cords, cordSchema)
-      resolve(validation)
-    }
-    return new Promise(asyncOp).catch(err => console.log(err))
-  }
 }
 
 module.exports = olio
