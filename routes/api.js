8096d56ba2715eb19ef872a2b43796ceaa4043bf
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-11-25 14:30:29 +0200

    Add lists router.

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 const tasks = require('./tasks.js')
+const lists = require('./lists.js')
 
 app.use('/tasks', tasks)
+app.use('/lists', lists)
 
 module.exports = app
