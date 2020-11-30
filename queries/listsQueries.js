22ebb66f28a2da1e79f3d24cf1098788f3fe21d9
iskela45 <iskela45@gmail.com>
2020-11-30 12:41:29 +0200

    add apikey to listsQueries.validateKeys and bugfix createOrderClause

--- queries/listsQueries.js
+++ queries/listsQueries.js
@@ -3,7 +3,10 @@ const mysql = require('mysql')
 const olio = {
   validateKeys (keys) {
     const keyIsCorrect = (key) =>
-      key === 'created' || key === 'search_name' || key === 'sort'
+      key === 'created' ||
+      key === 'search_name' ||
+      key === 'sort' ||
+      key === 'apikey'
 
     return keys.every(keyIsCorrect)
   },
@@ -38,48 +41,47 @@ const olio = {
   createOrderClause (reqQuery) {
     let order = ' ORDER BY '
 
-    console.log(reqQuery)
-    console.log(reqQuery.sort)
-    console.log(typeof reqQuery)
-    const fsorts = reqQuery.sort.split(' ').join('+')
-    const sorts = fsorts.split(',')
+    if ('sort' in reqQuery) {
+      const fsorts = reqQuery.sort.toString().split(' ').join('+')
+      const sorts = fsorts.split(',')
 
-    for (const sort of sorts) {
-      if (order !== ' ORDER BY ') {
-        order = order + ','
+      for (const sort of sorts) {
+        if (order !== ' ORDER BY ') {
+          order = order + ','
+        }
+
+        switch (sort) {
+          // Is done
+          case '+name':
+            order = order + 'name ASC'
+            break
+          case '-name':
+            order = order + 'name DESC'
+            break
+          // Created
+          case '+created':
+            order = order + 'created ASC'
+            break
+          case '-created':
+            order = order + 'created DESC'
+            break
+          // Edited
+          case '+edited':
+            order = order + 'edited ASC'
+            break
+          case '-edited':
+            order = order + 'edited DESC'
+            break
+        }
       }
 
-      switch (sort) {
-        // Is done
-        case '+name':
-          order = order + 'name ASC'
-          break
-        case '-name':
-          order = order + 'name DESC'
-          break
-        // Created
-        case '+created':
-          order = order + 'created ASC'
-          break
-        case '-created':
-          order = order + 'created DESC'
-          break
-        // Edited
-        case '+edited':
-          order = order + 'edited ASC'
-          break
-        case '-edited':
-          order = order + 'edited DESC'
-          break
+      if (order.substring(order.length - 1, order.length) === ',') {
+        order = order.slice(0, -1)
       }
     }
 
     if (order === ' ORDER BY ') {
       order = ''
-    }
-
-    if (order.substring(order.length - 1, order.length) === ',') {
-      order = order.slice(0, -1)
     }
 
     return order
