c80958a855560ec9a179cc36a1d5909db747b550
iskela45 <iskela45@gmail.com>
2020-12-02 13:53:42 +0200

    rollback

--- queries/tasksQueries.js
+++ queries/tasksQueries.js
@@ -26,9 +26,9 @@ const olio = {
 
       if (i === keys.length - 1) {
         console.log(reqQuery[key])
-        if (reqQuery.key === 'notNull') {
+        if (reqQuery[key] === 'notNull') {
           where += key + ' IS NOT NULL'
-        } else if (reqQuery.key === 'null') {
+        } else if (reqQuery[key] === 'null') {
           where += key + ' IS NULL'
         } else {
           where += key + '=' + reqQuery[key]
