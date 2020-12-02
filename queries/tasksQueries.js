1bd5e68d5113a5f2578d805b4aca7694e695e305
iskela45 <iskela45@gmail.com>
2020-12-02 13:56:34 +0200

    test toString()

--- queries/tasksQueries.js
+++ queries/tasksQueries.js
@@ -26,9 +26,9 @@ const olio = {
 
       if (i === keys.length - 1) {
         console.log(reqQuery[key])
-        if (reqQuery[key] === 'notNull') {
+        if (reqQuery[key].toString() === 'notNull') {
           where += key + ' IS NOT NULL'
-        } else if (reqQuery[key] === 'null') {
+        } else if (reqQuery[key].toString() === 'null') {
           where += key + ' IS NULL'
         } else {
           where += key + '=' + reqQuery[key]
