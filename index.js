70500ec56d23ef345bde0fa03fb5a8da90a1e3b0
iskela45 <iskela45@gmail.com>
2020-12-28 12:48:30 +0200

    update frontend build

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ app.use(express.static('build'))
 const port = process.env.PORT || 8080
 const server = app.listen(port, () => {
   console.log(`Listening on port ${server.address().port}`)
+  console.log(process.env.apikey)
 })
